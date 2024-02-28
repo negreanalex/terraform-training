643350ebb2e35f4d49b0939b489bc5193723ae5c
Alex Negrean <alex.negrean@plexus.com>
2024-02-28 21:34:35 +0200

    Add Azure AD Auth

--- azure/backend-test/remote-backend.tf
+++ azure/backend-test/remote-backend.tf
@@ -6,22 +6,15 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate-dev-uksouth"
-    storage_account_name = "tfstatedev49"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+    use_azuread_auth = true
+    }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "resource_group" {
   location = "centralus"
-  name     = "rg-test-007"
+  name     = "rg-test-010"
 }
